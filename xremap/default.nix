33c4d310c5b7e80f6129e1ac3a545a86ee88fbf6
つくも <92912896+Thukumo@users.noreply.github.com>
2025-10-25 03:25:04 +0900

    xremapの設定

--- xremap/default.nix
+++ xremap/default.nix
@@ -1,5 +1,18 @@
-{ pkgs, ... }:
+{ config, pkgs, ... }:
 
 {
   home.packages = [pkgs.xremap];
+  xdg.configFile."xremap/config.yml".source = ./config.yml;
+  systemd.user.services.xremap = {
+    Unit = {
+      Description = "xremap auto start";
+    };
+    Service = {
+      Restart = "always";
+      ExecStart = "${pkgs.xremap}/bin/xremap ${config.home.homeDirectory}/.config/xremap/config.yml";
+    };
+    Install = {
+      WantedBy = [ "default.target" ];
+    };
+  };
 }
