d901cade402bed694458585e28b99077dcb0e04a
つくも <92912896+Thukumo@users.noreply.github.com>
2025-11-03 03:44:10 +0900

    config.ymlのソースを自動生成したやつに変更

--- xremap/default.nix
+++ xremap/default.nix
@@ -5,20 +5,58 @@
     xremap
     wtype
   ];
-  xdg.configFile."xremap/config.yml".source = ./config.yml;
-  home.file = {
-    "hoge.yaml".text = (import ./gen_yaml.nix { 
-      lib = lib; 
-      input = {
-        virtual_modifiers = ["Ctrl_R" "PROG1"];
-        modmap = [
-          {
-            name = "Global";
-          }
-        ];
-      };
-    });
-  };
+  xdg.configFile."xremap/config.yml".text = (import ./gen_yaml.nix { 
+    lib = lib; 
+    input = {
+      virtual_modifiers = ["Ctrl_R" "PROG1"];
+      modmap = [
+        {
+          name = "Global";
+          remap = {
+            Shift_L = {
+              alone = "GRAVE";
+              held = "Shift_L";
+              free_hold = true;
+            };
+            CapsLock = {
+              alone = "Esc";
+              held = "LEFTMETA";
+              free_hold = true;
+            };
+            Space = {
+              alone = "Space";
+              held = "PROG1";
+              free_hold = true;
+            };
+            GRAVE = "ALT_L";
+            Shift_R = "LEFTMETA";
+            Muhenkan = "Home";
+            Henkan = "End";
+            KatakanaHiragana = "End";
+          };
+        }
+      ];
+      keymap = [
+        {
+          name = "power";
+          exact_match = true;
+          remap = {
+            Ctrl-Alt-Shift-P = "POWER";
+            Ctrl-Alt-Shift-R = "RESTART";
+          };
+        }
+        {
+          name = "arrow";
+          remap = {
+            PROG1-H = "left";
+            PROG1-J = "down";
+            PROG1-K = "up";
+            PROG1-L = "right";
+          };
+        }
+      ];
+    };
+  });
   systemd.user.services.xremap = {
     Unit = {
       Description = "xremap auto start";
@@ -26,13 +64,9 @@
     Service = {
       Restart = "always";
       ExecStart = "${pkgs.xremap}/bin/xremap --watch ${config.home.homeDirectory}/.config/xremap/config.yml";
-      X-ReloadTriggers = [
-        (builtins.hashFile "sha256" ./config.yml)
-      ];
     };
     Install = {
       WantedBy = [ "default.target" ];
     };
   };
-
 }
