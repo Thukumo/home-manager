af7e010c0f4b1e4f2c903474d6954bd8efffb7b3
つくも <92912896+Thukumo@users.noreply.github.com>
2025-11-03 13:01:27 +0900

    yaml生成のコードをちょっと変えた

--- xremap/gen_yaml.nix
+++ xremap/gen_yaml.nix
@@ -1,8 +1,8 @@
 { lib, input }:
 let
-  yaml_generator = count: old_data: data:
+  yaml_generator = count: wasList: data:
     let
-      s1 = if builtins.isString data then "\"${data}\""
+      s = if builtins.isString data then "\"${data}\""
       else
         if builtins.isBool data then
           if data then "true"
@@ -11,11 +11,10 @@ let
           if builtins.isNull data then "null"
           else
             if builtins.isList data then
-              lib.concatStrings (builtins.map (x: "${lib.strings.replicate count "  "}- ${x}") (builtins.map (yaml_generator (count + 1) data) data))
+              lib.concatStringsSep "\n" (builtins.map (x: "${lib.strings.replicate count "  "}- ${x}") (builtins.map (yaml_generator (count + 1) true) data))
             else
               if builtins.isAttrs data then
-                lib.concatStrings (map (att: att.value) (lib.attrsets.attrsToList (builtins.mapAttrs (name: value: "${lib.strings.replicate count "  "}${name}: ${yaml_generator (count + 1) data value}") data)))
+                lib.strings.concatMapAttrsStringSep "\n" (name: value: "${lib.strings.replicate count "   "}${name}: ${yaml_generator (count + 1) false value}") data
               else builtins.toString data;
-      s = if builtins.isList data || builtins.isAttrs data then s1 else s1 + "\n";
-    in if 0 == count || !(builtins.isAttrs data) && !(builtins.isList data) then s else if builtins.isList old_data && builtins.isAttrs data then lib.strings.trimWith {start = true;} s else "\n" + s;
-in yaml_generator 0 "gomi" input
+    in if 0 == count || !(builtins.isAttrs data) && !(builtins.isList data) then s else if wasList && builtins.isAttrs data then lib.strings.trimWith {start = true;} s else "\n" + s;
+in yaml_generator 0 false input
